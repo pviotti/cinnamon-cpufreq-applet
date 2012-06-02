20210196717f14d4a419fd98d5e812d032ecf747
Michael Webster <miketwebster@gmail.com>
2012-06-01 23:31:53 -0400

    more work on settings

--- cpufreq@mtwebster/settings.py
+++ cpufreq@mtwebster/settings.py
@@ -1,20 +1,63 @@
 from gi.repository import Gtk
 from os.path import expanduser
+from matplotlib.colors import colorConverter
 
+class Namespace: pass
 
 parsed_settings = []
 
 # set defaults and initialize setting holders
-disp_style = 2
-digit_type = 0
-cpus = 0
-refresh = 2000
-width = 4
-bg_color = "#ffffff"
-low_color = "#00ff00"
-medium_color = "#ffff00"
-high_color = "#ff0000"
-text_color = "#ffffff"
+s = Namespace()
+s.disp_style = 2
+s.digit_type = 0
+s.cpus = 0
+s.refresh = 2000
+s.width = 4
+s.bg_color = "#ffffff"
+s.low_color = "#00ff00"
+s.medium_color = "#ffff00"
+s.high_color = "#ff0000"
+s.text_color = "#ffffff"
+
+
+
+
+
+
+def hexToR(h): return int((cutHex(h))[0,2],16)
+def hexToG(h): return int((cutHex(h))[2,4],16)
+def hexToB(h): return int((cutHex(h))[4,6],16)
+def cutHex(h): return h[1,7]
+def d2h(d):
+    h = d.toString(16)
+    return (h.length == 1) ? '0' + h : h
+
+
+def int2hexcolor(d):
+    b = d & 255
+    g = (d >> 8) & 255
+    r = (d >> 16) & 255
+    rgbhex = d2h(r) + d2h(g) + d2h(b)
+    return rgbhex
+
+
+function hexcolor2int(h) {
+    let r = hexToR(h);
+    let g = hexToG(h);
+    let b = hexToB(h);
+    let intcolor = (r << 16);
+    intcolor += (g << 8);
+    intcolor += b;
+    return intcolor;
+}
+
+
+
+
+
+
+
+
 
 
 def readSettings(settings_file_path):
@@ -53,16 +96,16 @@ def getSetting(key, default):
             return result
 
 def initSettings():
-    disp_style = int(getSetting("Display Style", "2"))
-    digit_type = int(getSetting("Digit Type", "0"))
-    cpus = int(getSetting("CPUs to Monitor", "0"))
-    refresh = int(getSetting("Refresh Time", "2000"))
-    width = int(getSetting("Graph Width", "4"))
-    bg_color = getSetting("Background", "#ffffff")
-    low_color = getSetting("Low Color", "#00ff00")
-    medium_color = getSetting("Med Color", "#ffff00")
-    high_color = getSetting("High Color", "#ff0000")
-    text_color = getSetting("Text Color", "#ffffff")
+    s.disp_style = int(getSetting("Display Style", "2"))
+    s.digit_type = int(getSetting("Digit Type", "0"))
+    s.cpus = int(getSetting("CPUs to Monitor", "2"))
+    s.refresh = int(getSetting("Refresh Time", "2000"))
+    s.width = int(getSetting("Graph Width", "4"))
+    s.bg_color = getSetting("Background", "#ffffff")
+    s.low_color = getSetting("Low Color", "#00ff00")
+    s.medium_color = getSetting("Med Color", "#ffff00")
+    s.high_color = getSetting("High Color", "#ff0000")
+    s.text_color = getSetting("Text Color", "#ffffff")
 
 class Handler:
     def onDeleteWindow(self, *args):
@@ -80,11 +123,30 @@ builder.add_from_file("settings.glade")
 builder.connect_signals(Handler())
 
 window = builder.get_object("dialog1")
+initSettings()
 
+# set the initial state of my settings in the dialog
+cpu_display_combo = builder.get_object("cpu_display_combo")
+cpu_display_combo.set_active(s.cpus)
 
+disp_style_combo = builder.get_object("disp_style_combo")
+disp_style_combo.set_active(s.disp_style)
 
+show_combo = builder.get_object("show_combo")
+show_combo.set_active(s.digit_type)
 
+graph_width_slider = builder.get_object("graph_width_slider")
+graph_width_slider.set_value(s.width)
+
+refresh_slider = builder.get_object("refresh_slider")
+refresh_slider.set_value(s.refresh)
+
+high_color = builder.get_object("high_color")
+high_color.set_rgba(colorConverter.to_rgba(high_color,alpha=1.0))
 
 window.show_all()
+
+
+
 
 Gtk.main()
