83a9700933f446da564d3316c9059643097066ee
Michael Webster <miketwebster@gmail.com>
2012-06-01 21:56:52 -0400

    working on python settings

--- cpufreq@mtwebster/settings.py
+++ cpufreq@mtwebster/settings.py
@@ -4,6 +4,19 @@ from os.path import expanduser
 
 parsed_settings = []
 
+# set defaults and initialize setting holders
+disp_style = 2
+digit_type = 0
+cpus = 0
+refresh = 2000
+width = 4
+bg_color = "#ffffff"
+low_color = "#00ff00"
+medium_color = "#ffff00"
+high_color = "#ff0000"
+text_color = "#ffffff"
+
+
 def readSettings(settings_file_path):
     file = open(settings_file_path)
     file_data = file.read()
@@ -27,6 +40,30 @@ def readSettings(settings_file_path):
 
 
 
+def getSetting(key, default):
+    if len(parsed_settings) == 0:
+        return default;
+    res = ""
+    for i in range(0, len(parsed_settings)):
+        if key == parsed_settings[i][0]:
+            res = this.parsed_settings[i][1]
+        if res == "":
+            return default
+        else:
+            return result
+
+def initSettings():
+    disp_style = int(getSetting("Display Style", "2"))
+    digit_type = int(getSetting("Digit Type", "0"))
+    cpus = int(getSetting("CPUs to Monitor", "0"))
+    refresh = int(getSetting("Refresh Time", "2000"))
+    width = int(getSetting("Graph Width", "4"))
+    bg_color = getSetting("Background", "#ffffff")
+    low_color = getSetting("Low Color", "#00ff00")
+    medium_color = getSetting("Med Color", "#ffff00")
+    high_color = getSetting("High Color", "#ff0000")
+    text_color = getSetting("Text Color", "#ffffff")
+
 class Handler:
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
@@ -39,17 +76,15 @@ userhome = expanduser("~")
 settings_file_path = userhome + "/.cinnamon/cpufreq@mtwebster/cpufreq.conf"
 readSettings(settings_file_path)
 
-
-
-
-
-
-
-
 builder.add_from_file("settings.glade")
 builder.connect_signals(Handler())
 
 window = builder.get_object("dialog1")
+
+
+
+
+
 window.show_all()
 
 Gtk.main()
